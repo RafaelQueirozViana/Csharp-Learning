da868023434cb883e2fee095da775c6857931bfe
rafael <rafaelviana.contact@gmail.com>
2026-06-07 13:49:05 -0300

    first commit, starting to learning c#

---
+++ CsharpCourseProgramming/CsharpBasics/CsharpBasics/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CsharpBasics {
+    internal class Program {
+        static void Main(string[] args) {
+
+            string username = "nicolas";
+            int age = 20;
+            double totalInAccount = 30000.99;
+            char genre = 'M';
+            long binaries = 30000000L;
+            bool isWorking = true;
+
+
+            Console.WriteLine($"The user {username} has ${totalInAccount} in his account and it's {isWorking}");
+
+        }
+    }
+}
