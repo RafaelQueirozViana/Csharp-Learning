e07edcdc412b08a97d18fd586b3178d21361244d
Rafaelq <rafaelviana.contact@gmail.com>
2026-06-09 13:55:24 -0300

    learning while loop

---
+++ CsharpFundamentals/loopingFunctions/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+   bool isRunning = true;
+
+         while(isRunning)
+            {
+                System.Console.WriteLine("Type a number");
+
+                int choosedNumber = int.Parse(Console.ReadLine());
+
+                if(choosedNumber >=0)
+                {
+                int result = choosedNumber * choosedNumber;
+                System.Console.WriteLine($"the result of the raiz is: {result}");
+                }
+
+                else
+                {
+                    System.Console.WriteLine("You put a invalid number, the program will end");
+                    isRunning = false;
+                }
+
+
+
+            }
