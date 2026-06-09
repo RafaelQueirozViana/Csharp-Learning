029a38ef4210f4ecb443aff1f7de164e1c678f87
Rafaelq <rafaelviana.contact@gmail.com>
2026-06-09 15:55:48 -0300

    learning for loop

--- CsharpFundamentals/loopingFunctions/Program.cs
+++ CsharpFundamentals/loopingFunctions/Program.cs
@@ -1,6 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+
+bool isRunning = true;
+
+while (isRunning)
+{
+    System.Console.WriteLine("How many numbers do you want to repeat");
+
+    int repeatTimes = int.Parse(Console.ReadLine());
+
+    double sum = 0;
+
+    for (int i = 1; i <= repeatTimes; i++)
+
+    {
+        System.Console.WriteLine($"type your number {i}: ");
+        sum += double.Parse(Console.ReadLine());
+    }
+
+    System.Console.WriteLine($"the sum result of this operation is: {sum}");
+
+    System.Console.WriteLine("Do you want to continue the program? y/n");
+    if (Console.ReadLine() != "y")
+    {
+        System.Console.WriteLine("program ended");
+        isRunning = false;
+    }
+}
+
+/*
    bool isRunning = true;
 
          while(isRunning)
@@ -17,10 +46,12 @@
 
                 else
                 {
-                    System.Console.WriteLine("You put a invalid number, the program will end");
+                    System.Console.WriteLine("You put a invalid number, the pr ogram will end");
                     isRunning = false;
                 }
 
 
 
-            }
+}
+
+*/
