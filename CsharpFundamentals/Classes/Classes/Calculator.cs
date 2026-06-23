d0ae17bbd2bbf83612ee60712b832f8e24ff7f99
RafaelQueirozViana <rafaelviana.contact@gmail.com>
2026-06-23 20:03:28 -0300

    Learning classes in C#

---
+++ CsharpFundamentals/Classes/Classes/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes {
+    public class Calculator {
+  
+        public double sumNumbers(double a, double b) {
+            return a + b;
+        }
+      
+
+
+
+    }
+}
---
+++ CsharpFundamentals/Classes/Classes/Program.cs
@@ -0,0 +1,73 @@
+﻿using Classes;
+using System;
+
+namespace MyApp {
+    internal class Program {
+        static void Main(string[] args) {
+
+            string choosedOperation;
+            double number1;
+            double number2;
+            bool continueRunning = true;
+
+            Calculator calculator = new Calculator();
+
+            Console.WriteLine("type the first number");
+            number1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("type the second number");
+            number2 = double.Parse(Console.ReadLine());
+
+           
+
+
+
+            while (continueRunning == true) {
+                continueRunning = false;
+
+                Console.WriteLine("Choose one operation: sum, subtract, multiply or divide");
+                choosedOperation = Console.ReadLine().ToLower();
+
+                if (choosedOperation == "sum") {
+                    Console.WriteLine($"The sum result is {calculator.sumNumbers(number1, number2)}");
+                }
+
+                else if (choosedOperation == "subtract") {
+
+
+                }
+                else if (choosedOperation == "multiply") {
+
+                }
+
+                else if (choosedOperation == "divide") {
+
+                }
+
+                else {
+                    Console.WriteLine("Choose a valid operation first");
+                    continueRunning = true;
+                }
+
+
+
+            }
+
+
+
+
+           
+
+
+
+
+
+
+
+            }
+
+      
+
+
+    }
+}
