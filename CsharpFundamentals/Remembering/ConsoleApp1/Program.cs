3ab91d94fe5bb75443eade46952c4346d916ce42
RafaelQueirozViana <rafaelviana.contact@gmail.com>
2026-07-09 16:08:31 -0300

    improving my knowledge about classes and static objects

---
+++ CsharpFundamentals/Remembering/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace ConsoleApp1 {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            Triangle triangle1 = new Triangle();
+            Triangle triangle2 = new Triangle();
+
+            Console.WriteLine("type the triangle 1 sizes");
+            triangle1.sizeA = double.Parse(Console.ReadLine());
+            triangle1.sizeB = double.Parse(Console.ReadLine());
+            triangle1.sizeC = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("type the triangle 2 sizes");
+            triangle2.sizeA = double.Parse(Console.ReadLine());
+            triangle2.sizeB = double.Parse(Console.ReadLine());
+            triangle2.sizeC = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(triangle1.GetArea());
+            Console.WriteLine(triangle2.GetArea());
+
+        }
+    }
+}
---
+++ CsharpFundamentals/Remembering/ConsoleApp1/Triangle.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ConsoleApp1 {
+    class Triangle {
+        public double sizeA;
+        public double sizeB;
+        public double sizeC;
+
+        public double GetArea() {
+
+            return sizeA * sizeB * sizeC;
+        }
+
+
+
+    }
+}
