bf02d41a9460e2279f01fca466debd9a1179b636
RafaelQueirozViana <rafaelviana.contact@gmail.com>
2026-07-12 15:05:50 -0300

    Learning Nullable Values

--- CsharpLogicProgramming/MemoryBehavior/Program.cs
+++ CsharpLogicProgramming/MemoryBehavior/Program.cs
@@ -1,6 +1,17 @@
 ﻿namespace MemoryBehavior {
     internal class Program {
+
+
+
+
         static void Main(string[] args) {
+
+
+            double? x = 13.0;
+
+            double y = x ?? 10.0;
+
+            Console.WriteLine(y);
 
         }
     }
