b3914062a3685e1e2296a0342154a1b7b77fd3bb
RafaelQueirozViana <rafaelviana.contact@gmail.com>
2026-07-18 10:48:48 -0300

    Average exercise using Vectors

--- CsharpLogicProgramming/Vectors/Program.cs
+++ CsharpLogicProgramming/Vectors/Program.cs
@@ -2,28 +2,20 @@
     internal class Program {
         static void Main(string[] args) {
 
-            Console.WriteLine("type how many numbers do you want to storage");
-            int totalNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Type how many people to see the height");
+            int totalPeople = int.Parse(Console.ReadLine());
 
-            double[] numsVect = new double[totalNum];
+            double[] heightVector = new double[totalPeople];
 
-            for (int i = 0; i < numsVect.Length; i++) {
-                Console.WriteLine($"type the number {i}: ");
-                numsVect[i] = double.Parse(Console.ReadLine());
-
+            for (int i = 0; i < heightVector.Length; i++) {
+                Console.WriteLine($"type the height of the person {i + 1}");
+                heightVector[i] = double.Parse(Console.ReadLine());
             }
-
-            Console.WriteLine("Storaged values on array:");
-            for (int i = 0; i < numsVect.Length; i++) {
-                Console.WriteLine(numsVect[i]);
-            }
-
-
-
-
-
-
+            Console.WriteLine("The average between these people is:");
+            Console.WriteLine(heightVector.Average());
 
         }
     }
+
+
 }
---
+++ CsharpLogicProgramming/Vectors/Theory.cs
@@ -0,0 +1,24 @@
+﻿namespace Vectors {
+    internal static class Theory {
+
+        public static void VectorsTheory() {
+
+            Console.WriteLine("type how many numbers do you want to storage");
+            int totalNum = int.Parse(Console.ReadLine());
+
+            double[] numsVect = new double[totalNum];
+
+            for (int i = 0; i < numsVect.Length; i++) {
+                Console.WriteLine($"type the number {i}: ");
+                numsVect[i] = double.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("Storaged values on array:");
+            for (int i = 0; i < numsVect.Length; i++) {
+                Console.WriteLine(numsVect[i]);
+            }
+
+        }
+    }
+}
