6a354d45512e4082cc9b6f026758970bac5bcb37
RafaelQueirozViana <rafaelviana.contact@gmail.com>
2026-07-18 10:34:12 -0300

    Learning vectors

--- CsharpLogicProgramming/Vectors/Program.cs
+++ CsharpLogicProgramming/Vectors/Program.cs
@@ -1,7 +1,29 @@
 ﻿namespace Vectors {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("type how many numbers do you want to storage");
+            int totalNum = int.Parse(Console.ReadLine());
+
+            double[] numsVect = new double[totalNum];
+
+            for (int i = 0; i < numsVect.Length; i++) {
+                Console.WriteLine($"type the number {i}: ");
+                numsVect[i] = double.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("Storaged values on array:");
+            for (int i = 0; i < numsVect.Length; i++) {
+                Console.WriteLine(numsVect[i]);
+            }
+
+
+
+
+
+
+
         }
     }
 }
