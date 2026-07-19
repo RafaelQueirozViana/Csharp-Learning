9c26ad3a41f65c825a36be62ff9f799fa56ea3b4
RafaelQueirozViana <rafaelviana.contact@gmail.com>
2026-07-19 09:42:52 -0300

    Learning Foreach

---
+++ CsharpLogicProgramming/Lists/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Lists {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int[] numbersArray = new int[] { 2, 3, 4 };
+
+            foreach (int currentNumber in numbersArray) {
+                Console.WriteLine(currentNumber);
+            }
+
+
+
+
+
+
+        }
+    }
+}
