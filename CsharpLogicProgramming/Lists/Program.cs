46007f633c917ee4510b1f1e96314f08d222399a
RafaelQueirozViana <rafaelviana.contact@gmail.com>
2026-07-19 10:58:08 -0300

    Learning Lists
    Learning List and the most used List Methods

--- CsharpLogicProgramming/Lists/Program.cs
+++ CsharpLogicProgramming/Lists/Program.cs
@@ -2,15 +2,39 @@
     internal class Program {
         static void Main(string[] args) {
 
-            int[] numbersArray = new int[] { 2, 3, 4 };
+            List<string> personsList = new List<string>() { "Nicolas", "Pedro", "Paulo", "Allex" };
 
-            foreach (int currentNumber in numbersArray) {
-                Console.WriteLine(currentNumber);
+            personsList.Add("João");
+            personsList.Insert(3, "Gabriel");
+
+            foreach (string currentValue in personsList) {
+                Console.WriteLine(currentValue);
             }
 
+            Console.WriteLine("------------");
+
+            string nameFounded = personsList.Find(currentValue => currentValue[0] == 'P');
+            Console.WriteLine(nameFounded);
 
 
+            int position = personsList.FindIndex(currentValue => currentValue == "Pedro");
+            Console.WriteLine($"Position First: {position}");
 
+            int position2 = personsList.FindLastIndex(currentValue => currentValue[0] == 'J');
+            Console.WriteLine($"Position Last: {position2}");
+
+
+            // Making a filter in the List
+
+            List<string> filteredPersons = personsList.FindAll(currentValue => currentValue.Length == 5);
+
+            Console.WriteLine("-------------");
+
+            Console.WriteLine("Filtered List with all names that have 5 chars:");
+
+            foreach (string currentValue in filteredPersons) {
+                Console.WriteLine(currentValue);
+            }
 
 
         }
