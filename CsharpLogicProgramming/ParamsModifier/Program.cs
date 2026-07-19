f18d6e899b6852d6ee7c90fc6f11814220186259
RafaelQueirozViana <rafaelviana.contact@gmail.com>
2026-07-19 09:00:48 -0300

    learned Params in methods

---
+++ CsharpLogicProgramming/ParamsModifier/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ParamsModifier {
+    internal class Program {
+        static void Main(string[] args) {
+
+
+
+            int value1 = Calculator.Sum(2, 3, 4);
+
+            int value2 = Calculator.Sum(2, 3, 4);
+
+            Console.WriteLine(value2);
+
+        }
+    }
+}
